c0be4b84afaaa5d3524f04fa0518f1575fbecc6a
Luis Romero <looes1@gmail.com>
2021-11-18 16:13:07 +1100

    add react page component, add links

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { Link } from 'gatsby'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Page</h1>
+      <Link to="/">Back to Index</Link>
+    </main>
+  )
+}
+
+export default AboutPage
