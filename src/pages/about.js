1ccc4779f53a170aa56fde3b361d5363d3923d93
Luis Romero <looes1@gmail.com>
2021-11-18 23:25:55 +1100

    add react component layout

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,11 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
+import Layout from '../components/layout'
 
 const AboutPage = () => {
   return (
-    <main>
-      <h1>About Page</h1>
-      <Link to="/">Back to Index</Link>
-    </main>
+    <Layout pageTitle="About Me">
+      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+    </Layout>
   )
 }
 
